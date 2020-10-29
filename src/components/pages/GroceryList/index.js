42d0570b15044c33102ba31a3f950422e169c5a7
Tim Lee <timlee@Tims-MacBook-Pro.local>
2020-10-29 15:35:15 -0700

    created GroceryList directory and index.js

---
+++ src/components/pages/GroceryList/index.js
@@ -0,0 +1,19 @@
+// import React
+// import router
+
+// import Firebase
+// import session user context and authorization
+
+
+// create main component with context.consumer
+
+// initial state -> need to decide structure of data
+
+// add item to user and then to list object
+// -> grab user id
+// -> some update function
+
+
+
+//export listform with class 
+
